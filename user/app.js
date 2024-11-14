e2cd0a880520d2c0a0feb5ad37829c0ccd885793
sajith sathyan <sajithsathyan@sajiths-MacBook-Air.local>
2024-11-14 16:40:34 +0530

    user/app.j s updation

--- user/app.js
+++ user/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the User Microservice!');
+  res.send('Hello from the User Microservic....!');
 });
 
 app.listen(PORT, () => {
